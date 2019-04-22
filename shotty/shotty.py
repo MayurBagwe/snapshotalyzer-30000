e8f8887bc40163ddd0df97af54c80fd4fe1ac586
Bagwe <mayur.bagwe@capgemini.com>
2019-04-22 20:54:20 +0530

    Modified file to get more info on instances

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -7,17 +7,15 @@ ec2 = session.resource('ec2')
 
 @click.command()
 def list_instances():
-    "List EC2 instances"
-    for i in ec2.instances.all():
-        print(',', join((
-            i.id,
-            i.instance_type,
-            i.placement['AvailabilityZone'],
-            i.state['Name'],
-            i.public_dns_name)))
-        print(i)
-    return
+		'''List EC2 instances'''
+		for i in ec2.instances.all():
+			print(', '.join((
+				i.id,
+				  i.instance_type,
+			i.placement['AvailabilityZone'],
+			i.state['Name'],
+			i.public_dns_name)))
 
 
 if __name__ == '__main__':
-    list_instances()
+		list_instances()
